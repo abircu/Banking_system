0394d4bb3e2b375ae4e1ef2b3a49931db8a0b1f2
abircu <mdabircse4873@gmail.com>
2023-05-12 02:08:46 +0600

    hello js

---
+++ index.js
@@ -0,0 +1,5 @@
+const loginBtn = document.getElementById("login");
+loginBtn.addEventListener("click", () => {
+  const loginArea = document.getElementById("login-area");
+  loginArea.style.display = "none";
+});
