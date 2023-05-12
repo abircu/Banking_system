3b32d1f054be729ac159703b32778d830b404dad
abircu <mdabircse4873@gmail.com>
2023-05-12 15:41:10 +0600

    banking system added

--- index.js
+++ index.js
@@ -2,4 +2,42 @@ const loginBtn = document.getElementById("login");
 loginBtn.addEventListener("click", () => {
   const loginArea = document.getElementById("login-area");
   loginArea.style.display = "none";
+  const TnansactonArea = document.getElementById("transaction-area");
+  TnansactonArea.style.display = "block";
 });
+//for deposit button
+const Addeposit = document.getElementById("button");
+Addeposit.addEventListener("click", () => {
+  const depositNumber = getInputNumber("depositAmount");
+  // const depositAmount = document.getElementById("depositAmount").value;
+  // const depositNumber = parseFloat(depositAmount);
+
+  update("currentBalance", depositNumber);
+  update("currentDeposit", depositNumber);
+
+  document.getElementById("depositAmount").value = "";
+});
+
+//withdraw button Even handler
+const withdrew = document.getElementById("withdraw");
+withdrew.addEventListener("click", () => {
+  // const withdrawAmount = document.getElementById("withdeawAmout").value;
+  const withdrawNumber = getTnputNumber("withdeawAmout");
+
+  withdrawNumber = parseFloat(withdrawAmount);
+
+  moneyOut("withdeawAmout", withdrawNumber);
+});
+
+function getInputNumber(id) {
+  const Amount = document.getElementById(id).value;
+  const amountNumber = parseFloat(Amount);
+  return amountNumber;
+}
+
+function update(id, depositNumber) {
+  const current = document.getElementById(id).innerText;
+  const currentNumber = parseFloat(current);
+  const totalAmount = depositNumber + currentNumber;
+  document.getElementById(id).innerText = totalAmount;
+}
